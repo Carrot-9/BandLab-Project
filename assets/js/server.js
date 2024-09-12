ba7e9b761b0de2361e5effa214b236464a1c3fc5
Krod-9 <KerrodMcNeal2@gmail.com>
2024-09-12 19:46:33 -0400

    Added 'TamagotchiContainer' class

--- assets/js/server.js
+++ assets/js/server.js
@@ -4,9 +4,10 @@ module.exports = http;
 
 const server = http.createServer((req, res) => {
 
+
     res.end();
 });
 
 server.listen(3000, () => {
-    console.log("Server is Running.");
+    console.log("Server is Running on Port 3000.");
 });
