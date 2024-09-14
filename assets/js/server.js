10023805fd87cac40fbfdbfa8a567c64a29f0a45
Krod-9 <KerrodMcNeal2@gmail.com>
2024-09-13 23:51:14 -0400

    Created 'tamagotchidb' mySql database

--- assets/js/server.js
+++ assets/js/server.js
@@ -4,9 +4,22 @@ const app = express();
 
 const PORT = 3000
 
-
 app.listen(
     PORT, 
     () => console.log(`Server is Running On http://localhost:${PORT}`)
 )
+
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'tamagotchidb'
+});
+
+db.connect((error) => {
+  if (error) throw error;
+  console.log('MySQL connected');
+});
 
