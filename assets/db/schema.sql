a8fdac05558a7b3089bdecb3df5bf675068eaa8a
Krod-9 <KerrodMcNeal2@gmail.com>
2024-09-15 13:07:21 -0400

    Added 'age_hourly', 'cleanup_old_age' and 'hunger_hourly' Events

--- assets/db/schema.sql
+++ assets/db/schema.sql
@@ -1,13 +1,52 @@
 USE tamagotchidb;
 
-CREATE TABLE tamagotchi_hunger (
+CREATE TABLE IF NOT EXISTS tamagotchi_stats (
 
 id INT AUTO_INCREMENT PRIMARY KEY,
 
+health INT DEFAULT 100,
+
 hunger INT DEFAULT 50,
 
-created_at TIMESTAMP
+happiness_level INT DEFAULT 0,
+
+friendship_level INT DEFAULT 0,
+
+age INT DEFAULT 0,
+
+weight FLOAT DEFAULT 5,
+
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 );
+
+SET GLOBAL event_scheduler = ON;
+
+ALTER EVENT age_hourly
+    ON SCHEDULE EVERY 1 HOUR
+    DO 
+        UPDATE tamagotchi_stats
+        SET age = age + 1
+        WHERE id = 1;
+
+ALTER EVENT cleanup_old_age
+    ON SCHEDULE EVERY 1 DAY 
+    STARTS '2024-9-16 00:00:00'
+    DO
+        UPDATE tamagotchi_stats
+        SET age = 0
+        WHERE age > 100;
+
+CREATE EVENT hunger_hourly
+    ON SCHEDULE EVERY 1 HOUR
+    DO 
+        UPDATE tamagotchi_stats
+        SET hunger = hunger - 1
+        WHERE id = 1;
+
+
+
+
+
 
 
--- assets/js/server.js
+++ assets/js/server.js
@@ -18,11 +18,25 @@ const db = mysql.createConnection({
   user: 'root',
   password: process.env.password,
   database: 'tamagotchidb'
-});
+}).promise();
 
 db.connect((error) => {
   if (error) throw error; 
   console.log('MySQL connected');
 });
+
+async function ShowStats() {
+const [rows] = await db.query("SELECT * FROM tamagotchi_stats");
+return rows; 
+  }
+
+  (async () => {
+const hunger = await ShowStats();
+console.log(hunger);
+  })().catch(err => {
+    console.error("Error has occured:", err)
+  });
+
+  app.get(/tamagotchi_stats/id)
 
 
