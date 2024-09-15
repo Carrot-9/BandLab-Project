f323fe498888dd664d731c33801844df3d9465a1
Krod-9 <KerrodMcNeal2@gmail.com>
2024-09-14 23:05:34 -0400

    Added 'tamagotchi-hunger' table

--- assets/db/schema.sql
+++ assets/db/schema.sql
@@ -2,11 +2,12 @@ USE tamagotchidb;
 
 CREATE TABLE tamagotchi_hunger (
 
-id INT AUTO_INCREMENT PRIMARY KEY;
+id INT AUTO_INCREMENT PRIMARY KEY,
 
-hunger INT DEFAULT 50;
+hunger INT DEFAULT 50,
 
-created_at TIMESTAMP;
+created_at TIMESTAMP
 
 );
+
 
