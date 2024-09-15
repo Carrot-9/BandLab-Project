fb3974c3378f9fb1456b0620c76b89a55fb8dc97
Krod-9 <KerrodMcNeal2@gmail.com>
2024-09-15 16:01:41 -0400

    Connected Nodejs Server to MySql

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,10 +1,33 @@
 const express = require('express'); 
-const mysql = require('mysql2');
-require('dotenv').config();
-
 const app = express();
 
 const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/api/hunger/:id', (req,res) => {
+  const id = req.params.id;
+  db.query('SELECT hunger FROM tamagotchi_stats WHERE id = ?', [id],(error, results) => {
+    if(error) {
+      console.error("Database Connection Error")
+      return res.status(500).json({error: 'Database query error'});
+    }
+    if(results.length === 0) {
+      console.error("hunger does not exist.")
+      return res.status(404).json({error: 'hunger does not exist.'});
+    }
+    res.json(results);
+  });
+});
+
+app.listen(
+  PORT, 
+  () => console.log(`Server is Running On http://localhost:${PORT}`)
+)
+
+const mysql = require('mysql2');
+
+require('dotenv').config();
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -24,35 +47,11 @@ return rows;
   }
 
   (async () => {
-const hunger = await ShowStats();
-console.log(hunger);
+const stats = await ShowStats();
+console.log(stats);
   })().catch(err => {
     console.error("Error has occured:", err)
   });
-
-  app.get('/' , (req, res) => {
-    res.send("Hello World!")
-  });
-
-  app.get('/hunger/:id', (req,res) => {
-    const id = req.params.id;
-    db.query('SELECT hunger FROM tamagotchi_stats WHERE id = ?', [id],(error, results) => {
-      if(error) {
-        console.error("Database Connection Error")
-        return res.status(500).json({error: 'Database query error'});
-      }
-      if(results.length === 0) {
-        console.error("hunger does not exist.")
-        return res.status(404).json({error: 'hunger does not exist.'});
-      }
-      res.json(results[0]);
-    });
-  });
-
-  app.listen(
-    PORT, 
-    () => console.log(`Server is Running On http://localhost:${PORT}`)
-)
 
 
 
